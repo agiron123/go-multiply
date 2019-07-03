7d94ee0484b7bad8d7b72b637b7c593ddef2ad1f
Andre Giron <agiron123@gmail.com>
2019-07-03 17:33:59 -0400

    Implement Multiply Naive and fix failing test. Resolves #2 and Resolves #3.

--- src/main.go
+++ src/main.go
@@ -5,10 +5,35 @@ package main
 
 import "fmt"
 
-func MultiplyNaive(a, b int)(result int) {
-	return 41
+func MultiplyNaive(a, b int) (product int) {
+	// First, check the signs of a and b.
+	isPositive := true
+	if (a < 0 && b > 0) || (a > 0 && b < 0) { // Result is going to be negative
+		isPositive = false
+	}
+
+	// Set both b and a to be positive integers.
+	// Cheating for now, but we'll discuss how to do this with bitwise operators very soon!
+	if a < 0 {
+		a = -a
+	}
+
+	if b < 0 {
+		b = -b
+	}
+
+	var result int = 0
+	for i := 0; i < b; i++ {
+		result += a
+	}
+
+	if !isPositive {
+		return -result
+	}
+
+	return result
 }
 
 func main() {
-	fmt.Println("Hello from main!")
+	fmt.Println("6 * 7 is:", MultiplyNaive(6, 7))
 }
