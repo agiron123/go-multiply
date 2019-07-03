f857a91d311e947fa20a7b3aab68d86bf4686ace
Andre Giron <agiron123@gmail.com>
2019-07-03 15:25:28 -0400

    Initial commit. Sets up project structure and adds .gitignore

---
+++ src/helloworld.go
@@ -0,0 +1,10 @@
+// Andre Giron 2019
+
+// Package multiply multiplies two integers without using the multiplication or division operators.
+package multiply
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go Multiply!!!")
+}
